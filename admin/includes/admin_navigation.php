4b03d2d1d7c25d35d8ba77c871e8e391794e4bc4
iruix <43129551+iruix@users.noreply.github.com>
2020-09-19 17:39:40 +0300

    Added Dynamic Users Online feature using AJAX

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -12,8 +12,8 @@
             </div>
             <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">
-            <li><a href="../index.php">Users Currently Online:<?php echo usersOnline(); ?></a></li>
-            <li><a href="../index.php">Home</a></li>
+                <li><a href="">Users Online: <span class="usersonline"></span></a></li>
+                <li><a href="../index.php">Home</a></li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $_SESSION['firstname'] . " " . $_SESSION['lastname']; ?> <b class="caret"></b></a>
                     <ul class="dropdown-menu">
